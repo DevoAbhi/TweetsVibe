39989a04f43288aa0aee103f3a41a6672dfae4cf
AbhiDroid <abhinabroy2001@gmail.com>
2023-08-24 23:17:01 +0530

    create a action to dispatch

---
+++ src/stateManager/actionCreators/index.js
@@ -0,0 +1,75 @@
+import * as api from '../../api/index.js';
+
+/* basic syntax is ->
+    action function which returns a function which takes dispatch as arg.
+    now dispatch the type and payload
+    easy.
+*/
+
+
+export const signupUser = (formData, navigate) => async (dispatch) => {
+    try {
+        const {data} = await api.signup(formData);
+
+        dispatch({
+            type: "AUTH",
+            payload: data
+        })
+
+        navigate("/dashboard");
+    }
+
+    catch (error) {
+        console.log("Error while signing up -> ", error);
+    }
+}
+
+export const loginUser = (formData, navigate) => async (dispatch) => {
+    try {
+        const {data} = await api.login(formData);
+        console.log(data);
+
+        dispatch({
+            type: "AUTH",
+            payload: data
+        })
+
+        navigate("/dashboard");
+    }
+
+    catch (error) {
+        console.log("Error while logging in -> ", error);
+    }
+}
+
+export const logoutUser = (navigate) => (dispatch) => {
+    try {
+        dispatch({
+            type: "LOGOUT"
+        })
+
+        navigate("/auth");
+    }
+
+    catch (error) {
+
+    }
+}
+
+export const search = (formData) => async (dispatch) => {
+    try {
+        const {data} = await api.search(formData);
+        console.log(data);
+
+        dispatch({
+            type: "SEARCH",
+            payload: data
+        })
+
+        // navigate("/dashboard");
+    }
+
+    catch (error) {
+        console.log("Error while searching word -> ", error);
+    }
+}
