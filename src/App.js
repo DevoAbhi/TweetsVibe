9cd60969d44604ec87b9a5bb5445681f3e911ec5
AbhiDroid <abhinabroy2001@gmail.com>
2023-08-24 23:27:49 +0530

    update app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
+
+import Navbar from './components/Navbar/Navbar';
+import Auth from './components/Auth/Auth';
+import Dashboard from './components/Dashboard/Dashboard';
 
 
 
 function App() {
   return (
     <Router>
+      {/* {showNavbar && <Navbar />} */}
+      <Navbar />
       <Routes>
-        {/* <Route path="/product" exact element={<Product />} /> */}
+        {/* <Route path="/" exact element={<Home />} /> */}
+
+        {/* <Route path="/game" exact element={<Game />} />
+
+        <Route path="/card" exact element={<Card />} /> */}
+
+        <Route path="/auth" exact element={<Auth />} />
+
+        <Route path='/dashboard' exact element={<Dashboard />}/>
       </Routes>
     </Router>
   );
