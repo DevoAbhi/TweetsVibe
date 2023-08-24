acc2fc3848bbc015d7d7236751826f845ee6100f
AbhiDroid <abhinabroy2001@gmail.com>
2023-08-24 23:22:19 +0530

    .

---
+++ src/api/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+// const API = axios.create({ baseURL: 'https://trumpcard-backend.onrender.com' });
+const API = axios.create({ baseURL: 'http://localhost:5000' });
+
+API.interceptors.request.use((req) => {
+    if(localStorage.getItem("profile")) {
+        req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token}`;
+    }
+
+    return req;
+})
+
+
+// user login/signup apis
+export const signup = async (userData) => await API.post('/api/signup', userData);
+export const login = async (userData) => await API.post('/api/login', userData);
+export const search = async (searchData) => await API.post('/api/scrap-twitter', searchData);
