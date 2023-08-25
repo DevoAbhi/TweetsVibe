b74a625deffcd2583fc6e1c87054803449f4bc43
AbhiDroid <abhinabroy2001@gmail.com>
2023-08-26 00:27:19 +0530

    add charts to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import Chart from './Chart';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllSearchData } from '../../stateManager/actionCreators';
+import './Dashboard.css'
 
 function Dashboard() {
+  const dispatch = useDispatch();
+  const {searchData} = useSelector((state) => state.search);
+
+  useEffect(() => {
+    dispatch(getAllSearchData());
+  }, [dispatch]);
+
+  if(searchData) {
+
+    console.log(searchData.searchData);
+  }
+
   return (
-    <div>
-        
+    <div className='dashboard'>
+      {searchData && searchData.searchData && searchData.searchData.map(searchPayload => (
+        <Chart
+          key={searchPayload.id}
+          dailySentiments = {searchPayload.dailySentiments}
+          searchWord = {searchPayload.searchWord}
+        />
+      ))
+
+      }
+
     </div>
   )
 }
 
-export default Dashboard
+export default Dashboard;
